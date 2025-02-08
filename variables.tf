33c1b1ce07e8d096be6af29fcd87062fbc726aab
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-02-08 21:43:34 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "description" {
+  
+}
+
+variable "common_tags" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "sg_name" {
+  
+}
+
+variable "sg_tags" {
+    default = {}
+  
+}
